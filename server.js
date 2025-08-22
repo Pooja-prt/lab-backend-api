5c29e60d5ab67c4781c341f1d662a72935095cf1
Pooja-prt <2312033@nec.edu.in>
2025-08-22 14:48:05 +0530

    Added server.js, models, and routes for CRUD API

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// MongoDB Atlas Connection
+mongoose.connect(
+    'mongodb+srv://2312033:replace_here_with_encoded_password@cluster0.dtsu4pr.mongodb.net/labdb?retryWrites=true&w=majority&appName=Cluster0',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log("MongoDB connection error:", err));
+
+// Routes
+const itemRoutes = require('./routes/ItemRoutes');
+app.use('/api/items', itemRoutes);
+
+// Server listening
+app.listen(5000, () => console.log("Server running on port 5000"));
